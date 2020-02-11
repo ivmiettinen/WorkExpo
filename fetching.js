12f7bfac276b6951029ef2f874593a163edbaaa7
Ilkka Miettinen <ivmiettinen@gmail.com>
2020-02-11 10:40:15 +0200

    code cleaning

--- fetching.js
+++ fetching.js
@@ -1,11 +1,3 @@
-// function createNode(element) {
-//   return document.createElement(element); //Create the type of element you pass in the parametrs
-// }
-
-// function append(parent, el) {
-//   return parent.appendChild(el); //Append the second parameter(element) to the first one
-// }
-
 //AWS beanstalk URL:
 //awrest-env.sze2kbkguc.us-east-2.elasticbeanstalk.com/
 
@@ -14,13 +6,9 @@ const ul = document.getElementById('workplaces');
 const url = 'http://api.academicwork.net/api/adverts?country=fi';
 
 fetch(proxyURL + url)
-  .then(resp => resp.json()) //Transform the data into json
+  .then(resp => resp.json())
   .then(function(data) {
-    //Here you get the data to modify as you please
-
-    let workplaces = data; // Get the results
-
-    // console.log(workplaces);
+    let workplaces = data;
 
     const UusimaaWorkplaces = function(workplaces) {
       return (
@@ -52,8 +40,7 @@ fetch(proxyURL + url)
 
     function UusimaaNumber() {
       const OnlyUusimaa1 = workplaces.filter(UusimaaWorkplaces);
-      // console.log('OnlyUusimaa' + OnlyUusimaa1.length);
-      // console.log(OnlyUusimaa1);
+
       return OnlyUusimaa1.length;
     }
 
@@ -66,7 +53,6 @@ fetch(proxyURL + url)
       ' työpaikkaa.';
 
     function filterByLocationAndImg(workplaces) {
-      //Kaikki kaupungit uudelta maalta:
       const OnlyUusimaa2 =
         workplaces.Location === 'Espoo' ||
         workplaces.Location === 'Helsinki' ||
@@ -98,7 +84,6 @@ fetch(proxyURL + url)
           // console.log('Uusimaan kohteet: ' + workplaces.Location);
           return workplaces;
         } else {
-          //   console.log('ylimääräisiä kaupunkeja: ' + workplaces.Location);
         }
       }
     }
@@ -106,16 +91,6 @@ fetch(proxyURL + url)
     const OnlyUusimaa = workplaces.filter(filterByLocationAndImg);
 
     let MappingThrough = OnlyUusimaa.map(function(workplace) {
-      //Map throug the results and for each run the code below
-
-      // let li = createNode('li'), // Create the elements we need
-      //   img = createNode('img'),
-      //   span = createNode('span');
-      // div = createNode('div');
-
-      // span.innerHTML = `<img src="${workplace.LogoAbsoluteUrl}" >
-      //     Nimike: ${workplace.Title}: <br><br>`; //Make the HTML of our span to be the first and last name of our author
-
       return workplace;
     });
 
